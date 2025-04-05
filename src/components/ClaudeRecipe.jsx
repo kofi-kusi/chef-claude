2a048e2cbe05999b8d63fd988cfccb6c382e1afc
kusi <appaukusi05@gmail.com>
2025-04-05 20:23:48 +0000

    add a header for the ai response

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -4,6 +4,7 @@ export default function ClaudeRecipe(props) {
   return (
     <>
       <section className="suggested-recipe-container">
+        <h2>Chef Kusi recommendations:</h2>
         <ReactMarkdown>{props.recipe}</ReactMarkdown>
       </section>
     </>
